c0f5ccda2258f04091d98bc9afee5a01beb5073a
Breno <brenosousacst@gmail.com>
2023-06-01 22:26:22 -0300

    Arrumando requisição api

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,36 @@
-import React, {useState, useEffect} from 'react';
-import { getWeather } from './services';
-
-
+import {useState, useEffect} from 'react';
+import axios from 'axios';
+import { TextField } from '@mui/material';
 
 const App = () => {
-const [weather, setWeather] = useState(null)
-const [city, setCity] = useState('')
+const [data, setData] = useState({})
+const [city, setCity] = useState<string>('')
 
-const fetchData = async () => {
-    try{
-      const data = await getWeather(city)
-      console.log(data)
-    }catch(error){
-      console.log(error)
+const url = `https://api.openweathermap.org/data/2.5/weather?q=Anapolis&appid=26e47efb2756ac12c10a749aab657e47` 
+const searchLocation = async (event:React.KeyboardEvent<HTMLInputElement>) => {
+    if(event.key === "Enter"){
+      try{
+          axios.get(url).then((response) => {
+            console.log(response.data)
+          })
+      }catch(error){
+
+      }finally{
+        setCity('')
+      }
+
+
     }
   }
-  useEffect(()=>{
-    fetchData() 
-  },[])
+
  return <>
-    <input
-    type='text'
-    onChange={(e) => setCity(e.target.value)}/>
-    <button onClick={fetchData}/>
+    <TextField
+    type="text"
+    placeholder="Digite uma cidade"
+    value={city}
+    onChange={(e) => setCity(e.target.value)}
+    onKeyDown={searchLocation}/>
  </>
- 
 }
 
 export default App;
